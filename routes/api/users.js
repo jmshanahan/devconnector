a5e160e02574799e56d61f8815a0f2d22937747d
jmshanahan <comeraghsolutions@gmail.com>
2018-06-29 21:44:13 +0100

    Passport authentication implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const keys = require('../../config/keys');
 const User = require('../../models/User');
 // @route GET api/users/test
@@ -74,5 +75,21 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route GET api/users/current
+// @desc  Returns the current user
+// @access Private
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
