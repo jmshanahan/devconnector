87f27324ef466f7a4c33d0f6ee6aca5cc5364df9
jmshanahan <comeraghsolutions@gmail.com>
2018-07-17 11:51:28 +0100

    Updated readme file

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Introduction
+
+Partially completed Brad Traversy course on inplementing a MERN Stack front to back.
+
+This course provides an excellent example on how to implement JWT using PassportJWT.
+
+This will be my implementation of this going forward.
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -75,33 +75,41 @@ router.get('/user/:user_id', (req, res) => {
 // @desc  Create or Edit user profile
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateProfileInput(req.body);
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+  // const { errors, isValid } = validateProfileInput(req.body);
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
   const profileFields = {};
   profileFields.user = req.user.id;
-  if (req.body.handle) profileFields.handle = req.body.handle;
-  if (req.body.company) profileFields.company = req.body.company;
-  if (req.body.website) profileFields.website = req.body.website;
-  if (req.body.location) profileFields.location = req.body.location;
-  if (req.body.bio) profileFields.bio = req.body.bio;
-  if (req.body.status) profileFields.status = req.body.status;
-  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-  if (typeof req.body.skills !== 'undefined') {
-    if (req.body.skills) profileFields.skills = req.body.skills.split(',');
-  }
-  profileFields.social = {};
-  if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-  if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-  if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-  if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+  // if (req.body.handle) profileFields.handle = req.body.handle;
+  // if (req.body.company) profileFields.company = req.body.company;
+  // if (req.body.website) profileFields.website = req.body.website;
+  // if (req.body.location) profileFields.location = req.body.location;
+  // if (req.body.bio) profileFields.bio = req.body.bio;
+  // if (req.body.status) profileFields.status = req.body.status;
+  // if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+  // if (typeof req.body.skills !== 'undefined') {
+  //   if (req.body.skills) profileFields.skills = req.body.skills.split(',');
+  // }
+  // profileFields.social = {};
+  // if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+  // if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+  // if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+  // if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+  // if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
+  profileFields.handle = req.body.handle;
+  profileFields.company = 'my company';
+  profileFields.website = 'www.company.au';
+  profileFields.location = 'England';
+  profileFields.bio = 'my bio';
+  profileFields.status = 'Employed';
+  profileFields.skills = ['php', 'javascript', 'hurling'];
   Profile.findOne({ user: req.user.id }).then(profile => {
     if (profile) {
       // Update
-      Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+      // Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+      Profile.findOneAndUpdate({ handle: 'BradTrav' }, { $set: { website: 'www.joe.ie' } }, { new: true })
         .then(userprofile => res.json(userprofile))
         .catch(err => res.status(404).json(err));
     } else {
@@ -120,6 +128,6 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
         });
     }
   });
-  return res.status(400).json({ error: 'Unexpected error' });
+  //return res.status(400).json({ error: 'Unexpected error' });
 });
 module.exports = router;
